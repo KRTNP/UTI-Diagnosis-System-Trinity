f0f69fe7aed9eac7b0748ab0705c385bc74fd619
Nattaphon Honghin <154703717+KRTNP@users.noreply.github.com>
2024-12-05 22:55:13 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='uti_diagnosis_system',
     version='1.0.0',
-    author='Your Name',
+    author='Nattaphon Honghin',
+    author_email='nattaphon.honghin@gmail.com',
     description='An AI-based system for preliminary UTI diagnosis.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/KRTNP/UTI-Prediction-Trinity',
     packages=find_packages(),
     install_requires=[
         'numpy',
@@ -20,7 +24,16 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            # Add command-line scripts here if applicable
         ],
     },
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Healthcare Industry',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
+    python_requires='>=3.10',
+    include_package_data=True,
+    keywords='UTI diagnosis, machine learning, healthcare, AI'
 )
